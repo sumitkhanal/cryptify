e788bc2e18917650fa4f620df046b6d21fd9af77
sumitkhanal <sumit.khanal80@gmail.com>
2020-07-24 21:35:12 +0545

    some changes

--- decrypt page/global.js
+++ decrypt page/global.js
@@ -1,3 +1,14 @@
+//burger
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
 (function(){
 	"use strict";
 
